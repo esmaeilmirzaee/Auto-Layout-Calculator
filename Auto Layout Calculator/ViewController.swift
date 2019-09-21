efa2e8db9efc118548ff7eed7135f8146d8dd85c
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2019-09-21 17:00:04 +0430

    first init

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Esmaeil MIRZAEE on 2019-09-21.
+//  Copyright © 2019 Esmaeil MIRZAEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
